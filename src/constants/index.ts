1ba1c8c1b007ca328d6c9db5969a9305813edc39
AjayiMike <ajayimikeofficial@gmail.com>
2023-12-22 22:40:25 +0100

    feat: add uniswapV2FactoryAddress

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,11 @@
 const env_CHAIN_ID = process.env.NEXT_PUBLIC_CHAIN_ID;
-const env_RPC_URL = process.env.NEXT_PUBLIC_CHAIN_ID;
+const env_RPC_URL = process.env.NEXT_PUBLIC_RPC_URL;
 
 export const CHAIN_ID = env_CHAIN_ID ? (env_CHAIN_ID as unknown as number) : 1;
 export const RPC_URL = env_RPC_URL ?? "https://eth.llamarpc.com";
 
 export const uniswapInterfaceMulticallAddress =
     "0x1F98415757620B543A52E61c46B32eB19261F984";
+
+export const uniswapV2FactoryAddress =
+    "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
